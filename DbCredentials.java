c9e98f0ef78a5e54143b6ed1486e556da0db4a7f
koushikchaganti <koushik.chaganti369@gmail.com>
2016-09-25 23:19:43 -0500

    Add files via upload

---
+++ DbCredentials.java
@@ -0,0 +1,8 @@
+package com.koushikchaganti.pincode.db;
+
+public interface DbCredentials {
+	public String db_conn_str = "jdbc:oracle:thin:@mydatabase.c9nna2qkgrap.us-east-1.rds.amazonaws.com:1521:orcl";
+	public String db_user = "Koushik";
+	public String db_password = "Koushik93";
+}
+
