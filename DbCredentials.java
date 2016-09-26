bae5011a0b47e3391087ef49a24db1e1a22baf37
koushikchaganti <koushik.chaganti369@gmail.com>
2016-09-25 23:20:34 -0500

    Update DbCredentials.java

--- DbCredentials.java
+++ DbCredentials.java
@@ -1,8 +1,8 @@
 package com.koushikchaganti.pincode.db;
 
 public interface DbCredentials {
-	public String db_conn_str = "jdbc:oracle:thin:@mydatabase.c9nna2qkgrap.us-east-1.rds.amazonaws.com:1521:orcl";
-	public String db_user = "Koushik";
-	public String db_password = "Koushik93";
+	public String db_conn_str = "URL of Database";
+	public String db_user = "username";
+	public String db_password = "password";
 }
 
